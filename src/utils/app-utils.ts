3fc72ff25c3c5be8e0a85df389450b3752115948
BryceHamilton <bhamilton@haivision.com>
2021-01-16 23:02:54 -0500

    clean up

--- src/utils/app-utils.ts
+++ src/utils/app-utils.ts
@@ -24,20 +24,6 @@ export const asyncHandler = (fn: RequestHandler): RequestHandler => (
     res.status(500).json({ msg: 'Server Error', error: err });
   });
 
-export const asyncHandlerRedirect = (fn: RequestHandler): RequestHandler => (
-  req,
-  res,
-  next,
-): Promise<void> =>
-  Promise.resolve(fn(req, res, next)).catch(() => res.redirect('/'));
-
-const DAY_IN_MILLISECONDS = 24 * 60 * 60 * 1000;
-
-export const cookieParams = {
-  maxAge: DAY_IN_MILLISECONDS,
-  keys: [process.env.COOKIE_KEY || 'ZG3G7YN53ML4YLAAWQVDJVA'],
-};
-
 export const cors: RequestHandler = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Headers', 'content-type');
