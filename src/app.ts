507510b83691261c2e0464177f443b1cb692fb4f
BryceHamilton <bhamilton@haivision.com>
2021-01-18 14:43:28 -0500

    add cors origin

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://image-hub101.netlify.app'],
     credentials: true,
   }),
 );
